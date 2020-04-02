db7617fe19966b2e33bf5de6782d7cf0ce9c32a8
Toni  Diep <adiep123@gmail.com>
2020-04-02 03:45:58 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,9 +1,8 @@
 require 'pry'
-
 def my_find(collection)
-    i = 0
-    while i < collection.length
-      yield(collection[i])
+  i = 0
+  while i < 100
+    yield(1)
       i = i + 1
-    end
   end
+end
